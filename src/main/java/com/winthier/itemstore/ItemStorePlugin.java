4f20d082e57c83cfbb38422b2d662968e066dcae
StarTux <ths.loy@googlemail.com>
2021-10-07 20:00:08 +0200

    Replace deprecated references

--- src/main/java/com/winthier/itemstore/ItemStorePlugin.java
+++ src/main/java/com/winthier/itemstore/ItemStorePlugin.java
@@ -2,9 +2,6 @@ package com.winthier.itemstore;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -13,8 +10,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ItemStorePlugin extends JavaPlugin {
@@ -49,7 +44,7 @@ public final class ItemStorePlugin extends JavaPlugin {
 
     @Override
     public boolean onCommand(final CommandSender commandSender, final Command command, final String s, final String[] array) {
-        final Player player = (commandSender instanceof Player) ? (Player)commandSender : null;
+        final Player player = (commandSender instanceof Player) ? (Player) commandSender : null;
         try {
             if (array.length == 0) {
                 return false;
@@ -62,11 +57,11 @@ public final class ItemStorePlugin extends JavaPlugin {
                 if (!s2.matches("[a-zA-Z0-9-_]+")) {
                     throw new CommandException("Invalid item name: " + s2);
                 }
-                final ItemStack itemInHand = player.getItemInHand();
+                final ItemStack itemInHand = player.getInventory().getItemInMainHand();
                 if (itemInHand.getType() == Material.AIR) {
                     throw new CommandException("No item in your hand");
                 }
-                this.getStoredItems(true).set(s2, (Object)itemInHand.clone());
+                this.getStoredItems(true).set(s2, itemInHand.clone());
                 this.saveStoredItems();
                 player.sendMessage("" + ChatColor.YELLOW + "Item stored");
             } else if ("LoadItem".equalsIgnoreCase(s) && array.length == 1) {
@@ -83,27 +78,8 @@ public final class ItemStorePlugin extends JavaPlugin {
                 }
                 player.getInventory().addItem(new ItemStack[] {itemStack.clone()});
                 player.sendMessage("" + ChatColor.YELLOW + "Item given");
-            } else if ("FixBook".equalsIgnoreCase(s)) {
-                final ItemStack itemInHand2 = player.getItemInHand();
-                final BookMeta itemMeta = (BookMeta)itemInHand2.getItemMeta();
-                final List pages = itemMeta.getPages();
-                final ArrayList pages2 = new ArrayList<Object>(pages.size() / 2);
-                for (int i = 0; i < pages.size() / 2; ++i) {
-                    pages2.add(pages.get(i));
-                }
-                itemMeta.setPages((List)pages2);
-                itemInHand2.setItemMeta((ItemMeta)itemMeta);
-                commandSender.sendMessage("Book fixed");
             } else {
-                if (!"ListItems".equalsIgnoreCase(s) || array.length != 0) {
-                    return false;
-                }
-                final StringBuilder sb = new StringBuilder("Stored items:");
-                final Iterator<String> iterator = this.getStoredItems(true).getKeys(false).iterator();
-                while (iterator.hasNext()) {
-                    sb.append(" ").append(iterator.next());
-                }
-                commandSender.sendMessage("" + ChatColor.YELLOW + sb.toString());
+                return false;
             }
         } catch (CommandException ex) {
             commandSender.sendMessage("" + ChatColor.RED + ex.getMessage());
